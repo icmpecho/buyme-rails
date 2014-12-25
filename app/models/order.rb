844e74b7766e5a05ccb1c9465d8acfb5485f6182
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-12-25 12:30:53 +0700

    Add place order method

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,20 @@ class Order < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
   has_and_belongs_to_many :stores
+
+  def self.place( args )
+    user = args[:user]
+    item = args[:item]
+    stores = args[:stores]
+
+    order = self.new( user_id: user.id, item_id: item.id)
+
+    stores.each do |store|
+      order.stores << store
+    end
+
+    order.save ? order : nil
+
+  end
+
 end
