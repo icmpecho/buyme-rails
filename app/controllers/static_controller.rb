3721f539ec10605ee17771ab346a00b2c7a98fd0
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-12-25 15:45:38 +0700

    Remove app route and use / instead

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,10 @@
 class StaticController < ApplicationController
     def index
-        render :file => 'public/user.html', :layout => false
-    end
-
-    def app
+      if user_signed_in?
         render :file => 'public/app.html', :layout => false
+      else
+        render :file => 'public/user.html', :layout => false
+      end
     end
-
 end
 
