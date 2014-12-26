8b30420ee3ec94becc8d5d76c20dfd31da24a139
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-12-26 17:12:05 +0700

    Limit suggestion to 5 items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
 
   def self.suggest(name)
-    where("LOWER(name) LIKE ?", '%' + name.downcase + '%')
+    where("LOWER(name) LIKE ?", '%' + name.downcase + '%').limit(5)
   end
 
 end
