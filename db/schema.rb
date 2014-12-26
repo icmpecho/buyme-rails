2b4b3d44a19b0d099169429b7cc8f23f1117a091
Fai <fai@abctech-thailand.com>
2014-12-26 14:10:40 +0700

    Add column canceled_at into table orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225103824) do
+ActiveRecord::Schema.define(version: 20141226070540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20141225103824) do
     t.integer  "item_id"
     t.integer  "user_id"
     t.datetime "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "buyer_id"
+    t.datetime "canceled_at"
   end
 
   add_index "orders", ["buyer_id"], name: "index_orders_on_buyer_id", using: :btree
