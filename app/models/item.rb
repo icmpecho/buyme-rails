2945fcdb3ac04c9c3d59bf66d535f24dbad2a1ab
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-12-29 14:30:24 +0700

    Suggest now sort by top items

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ActiveRecord::Base
   }
 
   def self.suggest(name)
-    where("LOWER(name) LIKE ?", '%' + name.downcase + '%').limit(5)
+    where("LOWER(name) LIKE ?", '%' + name.downcase + '%').top.limit(5)
   end
 
 end
