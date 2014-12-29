389ec2e54d7db18b4654de75ea79f4be562e7fb2
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-12-29 16:47:59 +0700

    Fix broken count top item test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
+  before :each do
+    @seven  = Store.create( name: '7-11' )
+    @family = Store.create( name: 'family mart' )
+    @fai    = User.create( email: 'fai@abctech-thailand.com', password: '12345678', password_confirmation: '12345678' )
+    @ping   = User.create( email: 'ping@abctech-thailand.com', password: '12345678', password_confirmation: '12345678' )
+  end
+
   describe 'new' do
     it "should have a working constructor" do
       # setup
@@ -16,22 +23,23 @@ RSpec.describe Item, :type => :model do
       oishi = Item.create(name: 'oishi green tea')
       coke = Item.create(name: 'coke')
       matcha = Item.create(name: 'green tea flappe')
+      order1 = Order.place( user: @fai, item: oishi, stores: [ @seven, @family ] )
+      order2 = Order.place( user: @fai, item: coke, stores: [ @seven, @family ] )
+      order3 = Order.place( user: @fai, item: matcha, stores: [ @seven, @family ] )
 
       # act
       items = Item.suggest('green tea')
 
       # expect
-      expect(items.count).to eq 2
+      count = 0
+      items.each do |item|
+        count += 1
+      end
+      expect(count).to eq 2
     end
   end
 
   describe 'top' do
-    before :each do
-      @seven  = Store.create( name: '7-11' )
-      @family = Store.create( name: 'family mart' )
-      @fai    = User.create( email: 'fai@abctech-thailand.com', password: '12345678', password_confirmation: '12345678' )
-      @ping   = User.create( email: 'ping@abctech-thailand.com', password: '12345678', password_confirmation: '12345678' )
-    end
     it 'should return items order by number of orders' do
       # setup
       coke = Item.create(name: 'coke')
